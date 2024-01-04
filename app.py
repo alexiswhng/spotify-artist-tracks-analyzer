0b54ae08f84dd81d2dffd7706ce10877e1f5c349
Alexis Ng <57018350+alexiswhng@users.noreply.github.com>
2024-01-03 20:28:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,24 +46,34 @@ if artist_id:
     track_popularity = [track['popularity'] for track in top_tracks['tracks']]
     track_release_date = [track['album']['release_date'] for track in top_tracks['tracks']]
     track_audio = [track['external_urls']['spotify'] for track in top_tracks['tracks']]
+    track_album_cover = [track['album']['images'][0]['url'] for track in top_tracks['tracks']]
 
 # display the artist data in a table
-    st.image(BytesIO(r.content), width = 200)
+    # st.image(BytesIO(r.content), width = 200)
     st.write(f"## {name}")
     st.write()
     st.write("#### Top 10 Songs")
 
     for i in range(len(top_tracks['tracks'])):
-        # url = top_tracks['external_urls']['spotify']
-        st.write(f"**Track:** {track_names[i]}")
-        st.write(f"**Artists:** {track_artists[i]}")
-        st.write(f"**Release Date:** {track_release_date[i]}")
-        st.write(f"**Track Album:** {track_album[i]}")
-        st.write(f"**Track Duration:** {track_duration[i]} ms")
-        st.write(f"**Track Popularity:** {track_popularity[i]}")
-        st.write(f"**Audio:** {track_audio[i]} ")
-        st.write("Album Cover")
-        st.write("--")
+
+        st.write(" ")
+        col1, col2, col3 = st.columns([0.5,4,4])
+
+        with col1:
+            st.write(f"**{i+1}**")
+
+        with col2:
+            a = requests.get(track_album_cover[i])
+            st.image(BytesIO(a.content), width = 250)
+
+        with col3:
+            st.write(f"**Track**: [{track_names[i]}](%s)" % track_audio[i])
+            st.write(f"**Artists:** {track_artists[i]}")
+            st.write(f"**Release Date:** {track_release_date[i]}")
+            st.write(f"**Track Album:** {track_album[i]}")
+            st.write(f"**Track Duration:** {round((track_duration[i]/60000),2)} mins")
+            st.write(f"**Track Popularity:** {track_popularity[i]}")
+
 
 # create a dataframe from the playlist data
     data = {"Name": track_names, "Artist": track_artists, "Album": track_album, "Release Date": track_release_date, "Popularity": track_popularity, "Duration (ms)": track_duration}
@@ -73,33 +83,26 @@ if artist_id:
     fig_popularity = px.histogram(df, x="Popularity", nbins=20, title="Track Popularity Distribution")
     st.plotly_chart(fig_popularity)
 
-    
-# add a dropdown menu for bivariate analysis
-    st.write("#### Bivariate Analysis")
-    x_axis = st.selectbox("Select a variable for the x-axis:", ["Popularity", "Duration (ms)"])
-    y_axis = st.selectbox("Select a variable for the y-axis:", ["Popularity", "Duration (ms)"])
+# display scatter plot
+    st.write("#### Scatter Plot Analysis")     
+    x_axis = "Popularity"
+    y_axis = "Duration (ms)"
     fig_bivariate = px.scatter(df, x=x_axis, y=y_axis, title=f"{x_axis} vs. {y_axis}")
     st.plotly_chart(fig_bivariate)
 
 # add a dropdown menu for multivariate analysis
     st.write("#### Multivariate Analysis")
-    color_by = st.selectbox("Select a variable to color by:", ["Artist", "Album", "Release Date"])
+    color_by = st.selectbox("Select a variable to color by:", ["Name", "Album", "Release Date"])
     size_by = st.selectbox("Select a variable to size by:", ["Popularity", "Duration (ms)"])
     fig_multivariate = px.scatter(df, x="Duration (ms)", y="Popularity", color=color_by, size=size_by, hover_name="Name", title="Duration vs. Popularity Colored by Artist")
     st.plotly_chart(fig_multivariate)
 
-# add a summary of the playlist data
+# add a summary of the data
     st.write("")
-    st.write("### Playlist Summary")
+    st.write("### Artist Summary")
     st.write(f"**Most popular track:** {df.iloc[df['Popularity'].idxmax()]['Name']} by {df.iloc[df['Popularity'].idxmax()]['Artist']} ({df['Popularity'].max()} popularity)")
     st.write(f"**Least popular track:** {df.iloc[df['Popularity'].idxmin()]['Name']} by {df.iloc[df['Popularity'].idxmin()]['Artist']} ({df['Popularity'].min()} popularity)")
 
-# display a bar chart of the top 10 most popular artists in the playlist
-    st.write("#### Top 10 Artists")
-    st.write("The bar chart below shows the top 10 most popular artists in the playlist.")
-    top_artists = df.groupby("Artist").mean().sort_values("Popularity", ascending=False).head(10)
-    fig_top_artists = px.bar(top_artists, x=top_artists.index, y="Popularity", title="Top 10 Artists")
-    st.plotly_chart(fig_top_artists)    
 
 # display a bar chart of the top 10 most popular songs in the playlist
     st.write("#### Top 10 Songs")
@@ -107,4 +110,5 @@ if artist_id:
     top_artistss = df.groupby("Name").mean().sort_values("Popularity", ascending=False).head(10)
     fig_top_artistss = px.bar(top_artistss, x=top_artistss.index, y="Popularity", title="Top 10 Songs")
     st.plotly_chart(fig_top_artistss)
+
 
